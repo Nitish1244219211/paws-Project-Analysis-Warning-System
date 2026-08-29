baa01baacecf0486fbee520872da8b5384fd8a7c
Unknownguy <radhaswami124421@gmail.com>
2026-08-29 15:46:54 +0530

    deployment

--- backend/app.py
+++ backend/app.py
@@ -327,4 +327,4 @@ def predict_route():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
